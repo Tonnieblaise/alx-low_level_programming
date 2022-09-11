e0cbb60ed4e9c5210f180aecee287bf4c678bd0a
Tony <kachiblaise@gmail.com>
2022-09-10 17:02:11 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,11 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/*
+ *main - Entry point
+ * Description: Prints whether the number stored in
+ * n is positive or negative
+ */
 int main(void)
 {
 	int n;
@@ -10,13 +15,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if ( n > 0 ){
-		printf("%lu is positive, n\n");
+	if (n > 0)
+	{
+		printf("%u is positive, n\n");
 	}
-	else if ( n == 0 ){
+	else if (n == 0)
+	{
 		printf("%d is zero, n\n");
 	}
-	else {
+	else
+	{
 		printf("%d is negative, n\n");
 	}
 	return (0);
