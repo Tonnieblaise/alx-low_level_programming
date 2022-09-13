58bab0e4ecc5105a76da6a3dcd7355fdd4cd441c
Tony <kachiblaise@gmail.com>
2022-09-13 11:53:40 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: To print _putchar
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A repository of solutions on functions and nested loops in C programming
